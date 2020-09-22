1e083fe149ebeeb39e75366cb628fc0630bde9a0
Nikolai <nvanbaak@gmail.com>
2020-09-22 15:02:19 -0700

    search grabs weather information

--- weatherconsole.js
+++ weatherconsole.js
@@ -1,5 +1,8 @@
 const APIKEY = "&appid=94cca1029aaece9ee895c9d66c20117e";
 
+var responseObj;
+
+
 // search button functionality
 $("#city-submit").on("click", function(event) {
     event.preventDefault();
@@ -12,6 +15,8 @@ $("#city-submit").on("click", function(event) {
         method:"GET"
     }).then( function(response) {
 
+        responseObj = response;
+
         // Paste city name to Results
         var cityName = $("<h3>").text("——— " + response.name + " ———");
         $(".city-name").append(cityName);
@@ -20,7 +25,17 @@ $("#city-submit").on("click", function(event) {
         var date = $("<p>").text(moment().format("dddd, MMM do YYYY"));
         $(".date").append(date);
 
+        // Create weather "icon"
+        var weatherCondition = response.weather[0].description;
+        weatherCondition = $("<p>").text("The weather is: " + weatherCondition);
+        var preString = $("<p>").text("(icons take too long to load)");
+        var postString = $("<p>").text("(pretend I put one here)");
         
+        $(".weather-condition").append(preString);
+        $(".weather-condition").append(weatherCondition);
+        $(".weather-condition").append(postString);
+
+
 
 // an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 
